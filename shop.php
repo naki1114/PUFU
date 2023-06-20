2cc2f9513bc7c94fb4b3f2ced960f354f4dee941
Ubuntu <ubuntu@ip-172-31-45-96.ap-northeast-2.compute.internal>
2023-06-20 08:54:15 +0000

    웹 상점 틀짜기

---
+++ shop.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <title>포유뽀유 - 웹 상점</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="shop.css">
+
+  </head>
+
+  <?php
+    include_once 'base.php';
+
+    $_SESSION["page"] = "shop.php";
+  ?>
+
+  <body>
+
+    <div class="pufu-shop-row1">
+      <span>&nbsp;</span>
+      <span><h2>◈ 웹 상점</h2></span>
+      <span>&nbsp;</span>
+    </div>
+
+    <div class="pufu-shop-row2">
+      <span>&nbsp;</span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>&nbsp;</span>
+    </div>
+
+    <div class="pufu-shop-row2">
+      <span>&nbsp;</span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>
+        <div class="item-box">
+          <div class="goods1"></div>
+          <div>포켓몬 버즈 케이스</div>
+          <div>￦ 19,000</div>
+          <div>
+            <form method="post">
+              <input value="19000" style="display:none;">
+              <input type="button" class="shop-button indigo" value="구매하기">
+            </form>
+          </div>
+        </div>
+      </span>
+      <span>&nbsp;</span>
+    </div>
+
+    <div class="pufu-shop-row3">
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+    </div>
+
+  </body>
+
+</html>
